741e887086bbc844326f303e8fb588ba66de4fba
Simon Roberts <simon@dancingcloudservices.com>
2019-06-03 08:29:47 -0600

    Break one

---
+++ src/main/java/runnables/Visibility.java
@@ -0,0 +1,18 @@
+package runnables;
+
+public class Visibility {
+  static boolean stop = false;
+
+  public static void main(String[] args) throws Throwable {
+    new Thread(() -> {
+      System.out.println(Thread.currentThread().getName() + " starting");
+      while (! stop)
+        ;
+      System.out.println(Thread.currentThread().getName() + " ending");
+    }).start();
+    Thread.sleep(1_000);
+    System.out.println("main woke up, about to set stop to true.");
+    stop = true;
+    System.out.println("Main exiting...");
+  }
+}
